987d8f009ed838917973891e1a89f5968ddcc6d1
MeghnaPanda <36238300+MeghnaPanda@users.noreply.github.com>
2023-02-21 00:13:10 +1030

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,3 +48,13 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 
+add_my_fruit = requests.get("https://fruityvice.com/api/fruit/"+ "jackfruit")
+#streamlit.text(fruityvice_response)
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
+
+
