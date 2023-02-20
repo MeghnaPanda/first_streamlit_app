f433fde56e841cb8f9dca8d25a6b368187229771
MeghnaPanda <36238300+MeghnaPanda@users.noreply.github.com>
2023-02-21 00:29:21 +1030

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,10 +44,10 @@ except URLError as e:
   streamlit.error()
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('The user entered ', add_my_fruit)
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+#streamlit.write('The user entered ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 
